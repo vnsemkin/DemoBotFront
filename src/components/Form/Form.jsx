09b82400edb2c39682f2c6fd3f6d16db3e129fa3
Elena Soshina <soshinaalena6415@gmail.com>
2024-12-10 18:25:56 +0300

    add netlify.toml file for correct routing

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,11 +1,29 @@
 import './Form.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useTelegram } from '../../hooks/useTelegram'
 const Form = () => {
 	
 	const [country, setCountry] = useState('')
 	const [street, setStreet] = useState('')
 	const [subject, setSubject] = useState('physical')
 	
+	const {tg} = useTelegram()
+	
+	useEffect(() => {
+		return () => {
+			tg.MainButton.setParams({
+				text: 'Отправить данные'
+			})
+		}
+	}, [])
+	
+	useEffect(() => {
+		if(!street || !country) {
+			tg.MainButton.hide()
+		} else {
+			tg.MainButton.show()
+		}
+	}, [country, street])
 	const onChangeCountry = (e) => {
 		setCountry(e.target.value)
 	}
