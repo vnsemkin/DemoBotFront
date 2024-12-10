c54ca9c51c26badc75e2d8471c5782c1f6be057f
Elena Soshina <soshinaalena6415@gmail.com>
2024-12-10 18:16:45 +0300

    add form component

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,8 +1,45 @@
 import './Form.css'
+import { useState } from 'react'
 const Form = () => {
+	
+	const [country, setCountry] = useState('')
+	const [street, setStreet] = useState('')
+	const [subject, setSubject] = useState('physical')
+	
+	const onChangeCountry = (e) => {
+		setCountry(e.target.value)
+	}
+	
+	const onChangeStreet = (e) => {
+		setStreet(e.target.value)
+	}
+	
+	const onChangeSubject = (e) => {
+		setSubject(e.target.value)
+	}
+	
+	
 	return (
-		<div>
-			form
+		<div className='form'>
+			<h3>Введите ваши данные</h3>
+			<input
+			className={'input'}
+			type="text"
+			placeholder={'Страна'}
+			value={country}
+			onChange={onChangeCountry}/>
+			
+			<input
+			className={'input'}
+			type="text"
+			placeholder={'Улица'}
+			value={street}
+			onChange={onChangeStreet}/>
+			
+			<select value={subject} onChange={onChangeSubject} className={'select'}>
+				<option value={'physical'}>Физ.лицо</option>
+				<option value={'legal'}>Юр.лицо</option>
+			</select>
 		</div>
 	)
 }
