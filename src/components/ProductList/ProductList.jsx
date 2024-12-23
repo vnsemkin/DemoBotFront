55dd891545385b249b5a28ba7f0c4244280716a5
Elena Soshina <soshinaalena6415@gmail.com>
2024-12-23 19:06:54 +0300

    add queryId in useEffect deps

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -45,7 +45,7 @@ const ProductList = () => {
 			body: JSON.stringify(data)
 		})
 		
-	}, [addedItems])
+	}, [addedItems, queryId])
 	
 	useEffect(() => {
 		tg.onEvent('mainButtonClicked', onSendData)
