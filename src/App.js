691bc0c43479d4d9e4707f214e10c373cae15597
Elena <elena@192.168.10.4>
2025-02-19 14:45:33 +0700

    add HashRouter

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import { useEffect } from 'react'
+import { useEffect } from 'react';
 import './App.css';
-import { Route, Routes } from 'react-router-dom'
-import Form from './components/Form/Form'
-import Header from './components/Header/Header'
-import ProductList from './components/ProductList/ProductList'
-import { useTelegram } from './hooks/useTelegram'
+import { Route, Routes } from 'react-router-dom';
+import Form from './components/Form/Form';
+import Header from './components/Header/Header';
+import ProductList from './components/ProductList/ProductList';
+import { useTelegram } from './hooks/useTelegram';
 
 function App() {
-  const {tg} = useTelegram();
+    const { tg } = useTelegram();
 
     useEffect(() => {
         tg.ready();
-    }, []) // eslint-disable-line react-hooks/exhaustive-deps
-  
-  
-  return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route index element={<ProductList/>}/>
-        <Route path={'form'} element={<Form/>}/>
-      </Routes>
-    </div>
-  );
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+    return (
+        <div className="App">
+            <Header />
+            <Routes>
+                <Route path="/" element={<ProductList />} />
+                <Route path="/form" element={<Form />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
