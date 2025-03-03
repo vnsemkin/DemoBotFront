29c94df22ce20b4de3c3ef2bb2a4055a37c34524
ElenaSoshina <soshinaalena6415@gmail.com>
2025-03-03 18:21:00 +0700

    change

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
-import { useEffect } from 'react'
 import './App.css';
-import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
-import Form from './components/Form/Form'
-import Header from './components/Header/Header'
-import ProductList from './components/ProductList/ProductList'
-import { useTelegram } from './hooks/useTelegram'
+import {useEffect} from "react";
+import {useTelegram} from "./hooks/useTelegram";
+import Header from "./components/Header/Header";
+import {Route, Routes} from 'react-router-dom'
+import ProductList from "./components/ProductList/ProductList";
+import Form from "./components/Form/Form";
 
 function App() {
-    const { tg } = useTelegram();
+    const {onToggleButton, tg} = useTelegram();
 
     useEffect(() => {
         tg.ready();
-        tg.expand(); // Расширяет WebView до полной высоты
-    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+    }, [])
 
     return (
-        <Router basename="/telegram-web-app-react">
-            <div className="App">
-                <Header />
-                <Routes>
-                    <Route path="/" element={<ProductList />} />
-                    <Route path="/form" element={<Form />} />
-                </Routes>
-            </div>
-        </Router>
+        <div className="App">
+            <Header />
+            <Routes>
+                <Route index element={<ProductList />}/>
+                <Route path={'form'} element={<Form />}/>
+            </Routes>
+        </div>
     );
 }
 
