3e2200763c16ef4c2783d2ff740d463773872692
ElenaSoshina <soshinaalena6415@gmail.com>
2025-03-03 18:03:58 +0700

    c

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,58 +1,44 @@
-/* eslint-disable */
-import './ProductList.css'
-import { useCallback, useEffect, useState } from 'react'
-import { useTelegram } from '../../hooks/useTelegram'
-import ProductItem from '../ProductItem/ProductItem'
-import useCheckMixedContent from "../../hooks/useCheckMixedContent"
-import axios from 'axios'
+import React, {useState} from 'react';
+import './ProductList.css';
+import ProductItem from "../ProductItem/ProductItem";
+import {useTelegram} from "../../hooks/useTelegram";
+import {useCallback, useEffect} from "react";
 
 const products = [
-	{id: '1', title: 'Джинсы', price: 1500, description: 'Синего цвета, прямые'},
+	{id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
 	{id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
-	{id: '3', title: 'Джинсы 2', price: 1500, description: 'Синего цвета, прямые'},
-	{id: '4', title: 'Куртка 8', price: 1200, description: 'Зеленого цвета, теплая'},
-	{id: '5', title: 'Джинсы 3', price: 1500, description: 'Синего цвета, прямые'},
-	{id: '6', title: 'Куртка 7', price: 1800, description: 'Зеленого цвета, теплая'},
-	{id: '7', title: 'Джинсы 4', price: 1500, description: 'Синего цвета, прямые'},
-	{id: '8', title: 'Куртка 5', price: 15000, description: 'Зеленого цвета, теплая'},
+	{id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
+	{id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
+	{id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
+	{id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
+	{id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
+	{id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
 ]
 
 const getTotalPrice = (items = []) => {
-	return items.reduce((acc, item) => acc + item.price, 0)
+	return items.reduce((acc, item) => {
+		return acc += item.price
+	}, 0)
 }
 
 const ProductList = () => {
-	useCheckMixedContent()
-
-	const [addedItems, setAddedItems] = useState([])
-	const { tg, queryId } = useTelegram()
+	const [addedItems, setAddedItems] = useState([]);
+	const {tg, queryId} = useTelegram();
 
 	const onSendData = useCallback(() => {
-
-		if (!queryId) {
-			console.error('Error: queryId is missing');
-			return;
-		}
-
 		const data = {
 			products: addedItems,
 			totalPrice: getTotalPrice(addedItems),
 			queryId,
-		};
-
-		console.log('[DEBUG] Отправка данных GET:', data);
-
-		// Отправляем GET-запрос с данными в параметрах URL
-		axios.post('https://13d4-95-179-251-170.ngrok-free.app/web-data', data)
-
-			.then(response => {
-				console.log('[DEBUG] Ответ сервера:', response.data);
-			})
-			.catch(error => {
-				console.error('[ERROR] Ошибка запроса:', error.message);
-			});
-	}, [addedItems, queryId]);
-
+		}
+		fetch('http://95.179.251.170:8020/web-data', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(data)
+		})
+	}, [addedItems])
 
 	useEffect(() => {
 		tg.onEvent('mainButtonClicked', onSendData)
@@ -62,21 +48,21 @@ const ProductList = () => {
 	}, [onSendData])
 
 	const onAdd = (product) => {
-		const alreadyAdded = addedItems.find(item => item.id === product.id)
-		let newItems = []
+		const alreadyAdded = addedItems.find(item => item.id === product.id);
+		let newItems = [];
 
-		if (alreadyAdded) {
-			newItems = addedItems.filter(item => item.id !== product.id)
+		if(alreadyAdded) {
+			newItems = addedItems.filter(item => item.id !== product.id);
 		} else {
-			newItems = [...addedItems, product]
+			newItems = [...addedItems, product];
 		}
 
 		setAddedItems(newItems)
 
-		if (newItems.length === 0) {
-			tg.MainButton.hide()
+		if(newItems.length === 0) {
+			tg.MainButton.hide();
 		} else {
-			tg.MainButton.show()
+			tg.MainButton.show();
 			tg.MainButton.setParams({
 				text: `Купить ${getTotalPrice(newItems)}`
 			})
@@ -84,17 +70,16 @@ const ProductList = () => {
 	}
 
 	return (
-		<div className='list'>
+		<div className={'list'}>
 			{products.map(item => (
 				<ProductItem
-					key={item.id}
 					product={item}
 					onAdd={onAdd}
-					className='item'
+					className={'item'}
 				/>
 			))}
 		</div>
-	)
-}
+	);
+};
 
-export default ProductList
+export default ProductList;
