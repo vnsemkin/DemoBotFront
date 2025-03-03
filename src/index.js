63f491728cb086eadb4f1f3a14c2f95befffc77e
ElenaSoshina <soshinaalena6415@gmail.com>
2025-03-03 19:04:03 +0700

    add basename

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename="/telegram-web-app-react">
             <App />
         </BrowserRouter>
     </React.StrictMode>
