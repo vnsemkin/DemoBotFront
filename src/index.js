e0e4d0b03cc93cc9e89238d7c5e7c970d9ddb42c
ElenaSoshina <soshinaalena6415@gmail.com>
2025-03-03 19:11:19 +0700

    dscd

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
     <React.StrictMode>
-        <BrowserRouter basename="/telegram-web-app-react">
+        <BrowserRouter>
             <App />
         </BrowserRouter>
     </React.StrictMode>
